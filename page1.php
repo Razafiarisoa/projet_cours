38fc65bc45f3754f4a55076d170cad7f31dd7df0
Zolbrine <zolbrine.pro@outlook.fr>
2021-12-02 15:39:22 +0100

    ajout footer

--- page1.php
+++ page1.php
@@ -4,18 +4,26 @@
     <!--FIN MENUS-->
 
     <div class="esp">
-        <img class="tailleimg" class="img1" src="https://flagcdn.com/h40/za.png" alt="South Africa">
-        <img class="tailleimg" class="img2" src="https://flagcdn.com/h40/za.png" alt="South Africa">
-        <img class="tailleimg" class="img3" src="https://flagcdn.com/h40/za.png" alt="South Africa">
-        <img class="tailleimg" class="img4" src="https://flagcdn.com/h40/za.png" alt="South Africa">
-        <img class="tailleimg" class="img5" src="https://flagcdn.com/h40/za.png" alt="South Africa">
+        <img class="tailleimg img1" src="https://flagcdn.com/h40/za.png" alt="South Africa">
+        <img class="tailleimg img2" src="https://flagcdn.com/h40/za.png" alt="South Africa">
+        <img class="tailleimg img3" src="https://flagcdn.com/h40/za.png" alt="South Africa">
+        <img class="tailleimg img4" src="https://flagcdn.com/h40/za.png" alt="South Africa">
+        <img class="tailleimg img5" src="https://flagcdn.com/h40/za.png" alt="South Africa">
     </div>
 
     <hr>
 
     <footer>
-
+        <p id="COMS">Merci à toutes les équipes et à Juju!</p>
+        <div id="center">
+            <div id="dw16">
+                <img class="taillecoeur" src="images/coeur.png" alt="coeur">
+                <p id="pad" >DW16 Forever</p>
+                <img class="taillecoeur" src="images/coeur.png" alt="coeur">
+            </div>
+        </div>
     </footer>
+
 <script src="app.js"></script>
 </body>
 </html>
