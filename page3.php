e737014a038119a8d4b785287d944b213f8c29a1
Zolbrine <zolbrine.pro@outlook.fr>
2021-12-02 12:15:20 +0100

    ajouts nav php

--- page3.php
+++ page3.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Heures|Pays</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="logot">
-     <h1>Heures dans le Monde</h1>
-     <a href="index.php"><img class="taillelogo" id="pos" src="images/BP.jpg" alt="logo"></a>
-    </div>
-
-    <!--DEBUT MENUS-->
-
-    <nav>
-      <a href="page1.php">Asie</a>
-      <a href="page2.php">Europe</a>
-      <a href="page3.php">Amérique</a>
-      <a href="page4.php">Océanie</a>
-      <a href="page5.php">Afrique</a>
-    </nav>
+<?php include "nav.php";?>
 
     <hr>
     <!--FIN MENUS-->
