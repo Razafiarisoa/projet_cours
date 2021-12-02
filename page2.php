139ad4ce999db512a2083521f5b8da06c1118697
Zolbrine <zolbrine.pro@outlook.fr>
2021-12-02 10:34:11 +0100

    toutes les pages en place

---
+++ page2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heures|Pays</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="logot">
+     <h1>Heures dans le Monde</h1>
+     <a href="index.php"><img class="taillelogo" id="pos" src="images/BP.jpg" alt="logo"></a>
+    </div>
+
+    <!--DEBUT MENUS-->
+
+    <nav>
+      <a href="page1.php">Asie</a>
+      <a href="page2.php">Europe</a>
+      <a href="page3.php">Amérique</a>
+      <a href="page4.php">Océanie</a>
+      <a href="page5.php">Afrique</a>
+    </nav>
+
+    <hr>
+    <!--FIN MENUS-->
+
+    <div class="esp">
+        <img class="tailleimg" src="https://flagcdn.com/h40/za.png" srcset="https://flagcdn.com/h80/za.png 2x, https://flagcdn.com/h120/za.png 3x" height="40" alt="South Africa">
+        <img class="tailleimg" src="https://flagcdn.com/h40/za.png" srcset="https://flagcdn.com/h80/za.png 2x, https://flagcdn.com/h120/za.png 3x" height="40" alt="South Africa">
+        <img class="tailleimg" src="https://flagcdn.com/h40/za.png" srcset="https://flagcdn.com/h80/za.png 2x, https://flagcdn.com/h120/za.png 3x" height="40" alt="South Africa">
+        <img class="tailleimg" src="https://flagcdn.com/h40/za.png" srcset="https://flagcdn.com/h80/za.png 2x, https://flagcdn.com/h120/za.png 3x" height="40" alt="South Africa">
+        <img class="tailleimg" src="https://flagcdn.com/h40/za.png" srcset="https://flagcdn.com/h80/za.png 2x, https://flagcdn.com/h120/za.png 3x" height="40" alt="South Africa">
+    </div>
+
+    <hr>
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>
